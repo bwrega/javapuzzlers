02493f91a99cf56c8022eda05c0193a1413279be
Bartlomiej Wrega <bwrega@gmail.com>
2016-08-21 09:51:21 +0200

    01 - Oddity

---
+++ src/main/java/com/realizationtime/javapuzzlers/p01/OdditySolution.java
@@ -0,0 +1,9 @@
+package com.realizationtime.javapuzzlers.p01;
+
+public class OdditySolution {
+
+  public static boolean isOdd(int i) {
+    return i % 2 != 0;
+  }
+
+}
---
+++ src/test/java/com/realizationtime/javapuzzlers/p01/OddityTest.java
@@ -0,0 +1,32 @@
+package com.realizationtime.javapuzzlers.p01;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static com.realizationtime.javapuzzlers.p01.Oddity.isOdd;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class OddityTest {
+
+  @Test(dataProvider = "odds", enabled = false)
+  public void oddsAreHandledCorrectly(int n) throws Exception {
+    assertTrue(isOdd(n), n + " should be odd, but it isn't");
+  }
+
+  @Test(dataProvider = "evens")
+  public void evensAreHandledCorrectly(int n) throws Exception {
+    assertFalse(isOdd(n), n + " should not be odd, but it is");
+  }
+
+  @DataProvider(name = "odds")
+  public static Object[][] odds() {
+    return new Object[][]{{1}, {145}, {-1}, {-3}};
+  }
+
+  @DataProvider(name = "evens")
+  public static Object[][] evens() {
+    return new Object[][]{{0}, {2}, {146}, {-6}};
+  }
+
+}
